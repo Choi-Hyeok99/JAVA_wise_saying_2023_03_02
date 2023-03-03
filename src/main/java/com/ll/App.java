b9b6483fccfbed234716386790c6704b09b1d276
Choi-Hyeok99 <gur0709@naver.com>
2023-03-03 10:35:56 +0900

    Rq 전체 명령어에 도입

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -9,8 +9,6 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class App {
-
-
         public void run () {
 
             System.out.println("== 명언 앱 ==");
@@ -23,21 +21,21 @@ public class App {
             // trim() : 혹시 있을지 모를 좌우공백 제거된 버전으로 주세요
             String command = Container.getScanner().nextLine().trim();
 
-            if(command.equals("종료")) {
-                systemController.exit();
-                break;
-            } else if(command.equals("등록"))
-                {wiseSayingController.write();}
-              else if (command.equals("목록")) {
-                wiseSayingController.list();
-            } else if (command.startsWith("삭제")){
-                  // 정리 시작
-                Rq rq = new Rq(command);
-                  System.out.printf("actionCode: %s\n", rq.getActionCode());
-                  System.out.printf("params.id : %s\n", rq.getParam("id"));
+            Rq rq = new Rq(command);
 
-                  // 정리 끝
-                  wiseSayingController.remove();
+            switch (rq.getActionCode()) {
+                case "종료":
+                systemController.exit();
+                return;
+                case "등록":
+                wiseSayingController.write();
+                break;
+                case "목록":
+                wiseSayingController.list();
+                break;
+                case "삭제":
+                wiseSayingController.remove();
+                break;
             }
         }
     }
