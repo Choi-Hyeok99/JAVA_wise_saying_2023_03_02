c6bd15c260486513506ffedcc9fcc0bf27bfd18d
Choi-Hyeok99 <gur0709@naver.com>
2023-03-03 10:17:31 +0900

    6단계 Rq도입

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -32,20 +32,9 @@ public class App {
                 wiseSayingController.list();
             } else if (command.startsWith("삭제")){
                   // 정리 시작
-                  String[] commandBits = command.split("\\?",2);  // ? 기준으로 나눈다   최대 2까지
-                  String actionCode = commandBits[0];
-                  Map<String, String> params = new HashMap<>();
-                  String[] paramsBits = commandBits[1].split("&");
-
-                  for (String paramStr : paramsBits){
-                      String[] paramStrBits = paramStr.split("=",2);
-                      String key = paramStrBits[0];
-                      String value = paramStrBits[1];
-
-                      params.put(key, value);
-                  }
-                  System.out.printf("actionCode: %s\n", actionCode);
-                  System.out.printf("params : %s\n", params);
+                Rq rq = new Rq(command);
+                  System.out.printf("actionCode: %s\n", rq.getActionCode());
+                  System.out.printf("params.id : %s\n", rq.getParam("id"));
 
                   // 정리 끝
                   wiseSayingController.remove();
---
+++ src/main/java/com/ll/Rq.java
@@ -0,0 +1,37 @@
+package com.ll;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+// Rq는 Request(요청)이라는 의미
+public class Rq {
+
+    private  String actionCode;
+    private Map<String, String> params;
+    public Rq(String command){
+
+        String[] commandBits = command.split("\\?",2);  // ? 기준으로 나눈다   최대 2까지
+        actionCode = commandBits[0];
+
+        params = new HashMap<>();
+        String[] paramsBits = commandBits[1].split("&");
+
+        for (String paramStr : paramsBits){
+            String[] paramStrBits = paramStr.split("=",2);
+            String key = paramStrBits[0];
+            String value = paramStrBits[1];
+
+            params.put(key, value);
+        }
+    }
+
+    public String getActionCode() {
+        return actionCode;
+    }
+
+    public String getParam(String name){
+        return params.get(name);
+
+    }
+}
